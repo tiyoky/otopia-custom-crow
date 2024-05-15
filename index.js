9af568c708f7a9b1637760c8ea116dbd0be310bb
tiyoky <160074145+tiyoky@users.noreply.github.com>
2024-05-15 16:39:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -51,4 +51,14 @@ client.on('messageCreate', message => {
   }
 });
 
-client.login('VOTRE_BOT_TOKEN');
+async function login() {
+  try {
+    await client.login(process.env.TOKEN);
+    console.log(`\x1b[36m%s\x1b[0m`, `|    🐇 Logged in as ${client.user.tag}`);
+  } catch (error) {
+    console.error('Failed to log in:', error);
+    process.exit(1);
+  }
+}
+
+login()
