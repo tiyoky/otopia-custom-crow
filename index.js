1a8bf0015e741f924b1b11a4d30d87fd4da5d6b4
tiyoky <160074145+tiyoky@users.noreply.github.com>
2024-05-17 15:45:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { Client, GatewayIntentBits, Partials, EmbedBuilder, PermissionsBitField, ActionRowBuilder, ButtonBuilder, ButtonStyle, ChannelTypes, Intents } = require('discord.js');
+const { Client, GatewayIntentBits, Partials, EmbedBuilder, PermissionsBitField, ActionRowBuilder, ButtonBuilder, ButtonStyle, ChannelTypes, Intents, InteractionType } = require('discord.js');
+const axios = require('axios');
 
 const client = new Client({ 
   intents: [
@@ -16,12 +17,10 @@ const client = new Client({
 let prefix = '+';
 
 client.on('ready', () => {
-  console.log(`Connecté en tant que ${client.user.tag}!`);
+  console.log(`Connecté en tant que ${client.user.tag}!http://localhost:3000`);
   client.user.setActivity('made by tiyoky ❤️', { type: 'WATCHING' });
 });
 
-
-  
 client.on('messageCreate', async message => {
   if (message.author.bot || !message.content.startsWith(prefix)) return;
 
@@ -60,11 +59,11 @@ client.on('messageCreate', async message => {
 
   if (command === 'gcreate') {
     if (args.length < 4) {
-      return message.channel.send(`Usage: ${prefix}gcreate <titre> <description> <temp en ms> <nombre gagnant>`);
+      return message.channel.send(`Usage: ${prefix}gcreate <titre> <description> <temps en minutes> <nombre gagnant>`);
     }
 
     const [title, description, time, winnersCount] = args;
-    const duration = parseInt(time);
+    const duration = parseInt(time) * 60000; // Convertir les minutes en millisecondes
     const winnersNumber = parseInt(winnersCount);
 
     if (isNaN(duration) || isNaN(winnersNumber)) {
@@ -76,7 +75,7 @@ client.on('messageCreate', async message => {
       .setDescription(description)
       .addFields(
         { name: 'Nombre de gagnants', value: `${winnersNumber}`, inline: true },
-        { name: 'Temps', value: `${duration}ms`, inline: true }
+        { name: 'Temps', value: `${time} minutes`, inline: true } // Afficher le temps en minutes
       )
       .setColor('#FF0000')
       .setFooter({ text: 'Réagissez avec 🎉 pour participer!' });
@@ -108,27 +107,120 @@ client.on('messageCreate', async message => {
     }, duration);
   }
 
-  if (command === 'help') {
-    const helpEmbed = new EmbedBuilder()
-      .setColor('#FFFF00')
-      .setTitle('Menu d\'aide')
-      .setDescription('Voici les commandes disponibles :')
-      .addFields(
-        { name: `${prefix}prefix <nouveau préfixe>`, value: 'Change le préfixe du bot.' },
-        { name: `${prefix}help`, value: 'Affiche ce message d\'aide.' },
-        { name: `${prefix}mute <@user>`, value: 'Mute un utilisateur.' },
-        { name: `${prefix}unmute <@user>`, value: 'Unmute un utilisateur.' },
-        { name: `${prefix}kick <@user>`, value: 'Kick un utilisateur.' },
-        { name: `${prefix}ban <@user>`, value: 'Ban un utilisateur.' },
-        { name: `${prefix}gcreate <titre> <description> <temp en ms> <nombre gagnant>`, value: 'Crée un giveaway.' },
-        { name: `${prefix}stop`, value: 'stop le bot \(owner only\)' },
-        { name: `${prefix}say <msg>`, value: 'envoie le \(msg\) \(admin only\)' }
-      )
-      .setFooter({ text: 'made by tiyoky', iconURL: client.user.displayAvatarURL() });
+  if (command === 'kissorkill') {
+    if (!message.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+      return message.channel.send('Seuls les administrateurs peuvent utiliser cette commande.');
+    }
 
-    message.channel.send({ embeds: [helpEmbed] });
+    try {
+      const response = await axios.get('https://api.waifu.pics/sfw/waifu');
+      const imageUrl = response.data.url;
+
+      const kissOrKillEmbed = new EmbedBuilder()
+        .setTitle('**kiss or kill?**')
+        .setImage(imageUrl)
+        .setColor('#FF69B4');
+
+      const kissOrKillMessage = await message.channel.send({ embeds: [kissOrKillEmbed] });
+      await kissOrKillMessage.react('💋'); 
+      await kissOrKillMessage.react('🔪'); 
+    } catch (error) {
+      console.error('Erreur lors de la récupération de l\'image d\'anime:', error);
+      message.channel.send('Une erreur s\'est produite en récupérant l\'image d\'anime.');
+    }
   }
 
+
+  if (command === 'avatar') {
+    const user = message.mentions.users.first() || message.author;
+    const avatarEmbed = new EmbedBuilder()
+      .setTitle(`${user.username}'s Avatar`)
+      .setImage(user.displayAvatarURL({ dynamic: true, size: 4096 }))
+      .setColor('#008080');
+
+    message.channel.send({ embeds: [avatarEmbed] });
+  }
+
+  if (command === 'help') {
+    const helpEmbeds = [
+      new EmbedBuilder()
+        .setColor('#FFFF00')
+        .setTitle('Menu d\'aide - Page 1')
+        .setDescription('Voici les commandes disponibles :')
+        .addFields(
+          { name: `${prefix}prefix <nouveau préfixe>`, value: 'Change le préfixe du bot.' },
+          { name: `${prefix}help`, value: 'Affiche ce message d\'aide.' },
+          { name: `${prefix}mute <@user>`, value: 'Mute un utilisateur.' },
+          { name: `${prefix}unmute <@user>`, value: 'Unmute un utilisateur.' },
+          { name: `${prefix}kick <@user>`, value: 'Kick un utilisateur.' },
+          { name: `${prefix}ban <@user>`, value: 'Ban un utilisateur.' },
+          { name: `${prefix}gcreate <titre> <description> <temps en minutes> <nombre gagnant>`, value: 'Crée un giveaway.' }
+        )
+        .setFooter({ text: 'Page 1/2 - made by tiyoky', iconURL: client.user.displayAvatarURL() }),
+      new EmbedBuilder()
+        .setColor('#FFFF00')
+        .setTitle('Menu d\'aide - Page 2')
+        .setDescription('Voici les commandes disponibles :')
+        .addFields(
+          { name: `${prefix}stop`, value: 'Arrête le bot (owner only)' },
+          { name: `${prefix}restart`, value: 'restart le bot (owner only)' },
+          { name: `${prefix}say <msg>`, value: 'Envoie le message (admin only)' },
+          { name: `${prefix}avatar`, value: 'Affiche l\'avatar d\'un utilisateur.' },
+          { name: `${prefix}who`, value: 'Infos sur le bot.' },
+          { name: `${prefix}cat`, value: 'Affiche une image de chat aléatoire.' },
+          { name: `${prefix}dog`, value: 'Affiche une image de chien aléatoire.' },
+          { name: `${prefix}kissorkill`, value: 'envoie un embed kiss or kill (admin only)' }
+        )
+        .setFooter({ text: 'Page 2/2 - made by tiyoky', iconURL: client.user.displayAvatarURL() })
+    ];
+
+    const row = new ActionRowBuilder()
+      .addComponents(
+        new ButtonBuilder()
+          .setCustomId('previous')
+          .setLabel('⬅️')
+          .setStyle(ButtonStyle.Primary),
+        new ButtonBuilder()
+          .setCustomId('next')
+          .setLabel('➡️')
+          .setStyle(ButtonStyle.Primary)
+      );
+
+    const helpMessage = await message.channel.send({ embeds: [helpEmbeds[0]], components: [row] });
+
+    const filter = i => i.customId === 'previous' || i.customId === 'next';
+    const collector = helpMessage.createMessageComponentCollector({ filter, time: 60000 });
+
+    let currentPage = 0;
+    collector.on('collect', async i => {
+      if (i.customId === 'next') {
+        currentPage = (currentPage + 1) % helpEmbeds.length;
+      } else if (i.customId === 'previous') {
+        currentPage = (currentPage - 1 + helpEmbeds.length) % helpEmbeds.length;
+      }
+      await i.update({ embeds: [helpEmbeds[currentPage]], components: [row] });
+    });
+  }
+
+  
+  if (command === 'spam') {
+    if (!message.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+      return message.channel.send('Seuls les administrateurs peuvent utiliser cette commande.');
+    }
+
+    const [msg, times] = args;
+    const numberOfTimes = parseInt(times, 10);
+
+    if (!msg || isNaN(numberOfTimes) || numberOfTimes <= 0) {
+      return message.channel.send(`Usage: ${prefix}spam <message> <nombredefois>`);
+    }
+
+    for (let i = 0; i < numberOfTimes; i++) {
+      message.channel.send(msg);
+    }
+  }
+
+  
   if (command === 'say') {
     if (!message.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
       return message.channel.send('Seuls les administrateurs peuvent utiliser cette commande.');
@@ -136,6 +228,10 @@ client.on('messageCreate', async message => {
 
     const sayMessage = args.join(' ');
     message.channel.send(sayMessage);
+  }
+
+  if (command === 'who') {
+    return message.channel.send('Entièrement fait de A à Z par _tiyoky');
   }
 
   if (command === 'mute') {
@@ -159,26 +255,98 @@ client.on('messageCreate', async message => {
       return message.channel.send('Vous n\'avez pas les permissions pour unmute les membres.');
     }
     const user = message.mentions.members.first();
-    if  (!user) {
-            return message.channel.send('Veuillez mentionner un utilisateur à unmute.');
-          }
-          try {
-            await user.voice.setMute(false, 'Unmuted by command');
-            message.channel.send(`${user.user.tag} a été unmute.`);
-          } catch (err) {
-            message.channel.send('Impossible de unmute ce membre.');
-          }
-        }
+    if (!user) {
+      return message.channel.send('Veuillez mentionner un utilisateur à unmute.');
+    }
+    try {
+      await user.voice.setMute(false, 'Unmuted by command');
+      message.channel.send(`${user.user.tag} a été unmute.`);
+    } catch (err) {
+      message.channel.send('Impossible de unmute ce membre.');
+    }
+  }
 
-        if (command === 'stop') {
-          const ownerId = '1018206885704372274';
-          if (message.author.id !== ownerId) {
-            return message.channel.send('Seul le propriétaire du bot peut utiliser cette commande.');
-          }
+  if (command === 'restart') {
+    if (!message.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+      return message.channel.send('Seuls les administrateurs peuvent utiliser cette commande.');
+    }
 
-          message.channel.send('Arrêt du bot...');
-          client.destroy();
-        }
-      });
+    const ownerId = '1018206885704372274';
 
+    if (message.author.id !== ownerId) {
+      return message.channel.send('Seul le propriétaire du bot peut utiliser cette commande.');
+    }
+
+    message.channel.send('Redémarrage du bot...');
+    client.destroy();
+
+    setTimeout(() => {
       client.login(process.env.TOKEN);
+    }, 3000); 
+  }
+
+  
+  if (command === 'stop') {
+    const ownerId = '1018206885704372274';
+    if (message.author.id !== ownerId) {
+      return message.channel.send('Seul le propriétaire du bot peut utiliser cette commande.');
+    }
+
+    message.channel.send('Arrêt du bot...');
+    client.destroy();
+  }
+
+  if (command === 'cat') {
+    try {
+      const response = await axios.get('https://api.thecatapi.com/v1/images/search');
+      const catImageUrl = response.data[0].url;
+      const catEmbed = new EmbedBuilder()
+        .setTitle('Voici un chaton !')
+        .setImage(catImageUrl)
+        .setColor('#FF4500');
+      message.channel.send({ embeds: [catEmbed] });
+    } catch (error) {
+      console.error('Erreur lors de la récupération de l\'image de chat:', error);
+      message.channel.send('Une erreur s\'est produite en récupérant l\'image de chat.');
+    }
+  }
+
+  if (command === 'dog') {
+    try {
+      const response = await axios.get('https://dog.ceo/api/breeds/image/random');
+      const dogImageUrl = response.data.message;
+      const dogEmbed = new EmbedBuilder()
+        .setTitle('Voici un chien !')
+        .setImage(dogImageUrl)
+        .setColor('#FF4500');
+      message.channel.send({ embeds: [dogEmbed] });
+    } catch (error) {
+      console.error('Erreur lors de la récupération de l\'image de chien:', error);
+      message.channel.send('Une erreur s\'est produite en récupérant l\'image de chien.');
+    }
+  }
+
+  if (command === 'embedcreatefdfdfdfdfewfseifhsfsibfwaibsifjbaifbafafaijbfasibfasijbasifjbafijbasfijasbfiasjbfais') {
+    if (!message.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+      return message.channel.send('Seuls les administrateurs peuvent utiliser cette commande.');
+    }
+
+    const [title, description, imageUrl] = args;
+    if (!title || !description) {
+      return message.channel.send(`Usage: ${prefix}embedcreate <titre> <description> [imageurl]`);
+    }
+
+    const embed = new EmbedBuilder()
+      .setTitle(title)
+      .setDescription(description)
+      .setColor('#FF4500');
+
+    if (imageUrl) {
+      embed.setFooter({ text: 'Image de footer', iconURL: imageUrl });
+    }
+
+    message.channel.send({ embeds: [embed] });
+  }
+});
+
+client.login(process.env.TOKEN);
